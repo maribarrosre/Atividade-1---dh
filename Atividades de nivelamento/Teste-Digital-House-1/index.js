4a720e30e6d02c219bd439c0498e0c6d2cc319dd
Mariana Barros Rezende <96442362+maribarrosre@users.noreply.github.com>
2022-03-14 00:27:06 -0300

    Create index.js

---
+++ Atividades de nivelamento/Teste-Digital-House-1/index.js
@@ -0,0 +1,16 @@
+var moradores = [
+    "Fulano de Tal",
+    "Beltrano da Cia",
+    "Viajante do Tempo",
+    "Morador da Lua",
+    "Marciano Azul",
+    "Et da Eslováquia",
+    "Jedi do Lado Cinza da Força",
+    "Baby Yoda Amarelo"
+]
+  var numeroDoApartamento = []
+  for (var i = 0; i< numeroDoApartamento.length; i++){
+} if( numeroDoApartamento%2==0){
+  moradores.push(numeroDoApartamento[i])
+}
+console.log("O morador "+ moradores + " pode usar o elevador" )
